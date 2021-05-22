7c75a268476564f6e28e3bf5fba4fe7cd4716ec3
dodirizuan <dodirizuan@gmail.com>
2021-05-22 13:31:42 +0700

    Initial commit

---
+++ src/main/java/com/springapi/flickrfeed/FlickrfeedApplication.java
@@ -0,0 +1,13 @@
+package com.springapi.flickrfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickrfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickrfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapi/flickrfeed/FlickrfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapi.flickrfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickrfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
